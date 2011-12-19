0aeae7990572220271250cf579b89b716e3addbe
ryu22e <ryu22e@gmail.com>
2011-12-19 17:26:12 +0900

    Modify test/hooks.js.

--- test/hooks.js
+++ test/hooks.js
@@ -1,14 +1,45 @@
 describe('example - hooks', function(){
-  var setUp = false;
+  var beforeCount = 0,
+      beforeEachCount = 0,
+      afterEachCount = 0,
+      afterCount = 0;
 
-  beforeEach(function(done){
-    setUp = true;
+  before(function(done) {
+    beforeCount++;
     done();
   })
 
-  describe('test after beforeEach.', function(){
-    it('setUp is true.', function(){
-      setUp.should.equal(true);
+  beforeEach(function(done){
+    beforeEachCount++;
+    done();
+  })
+
+  afterEach(function(done) {
+    afterEachCount++;
+    done();
+  });
+
+  after(function(done) {
+    afterCount++;
+    afterCount.should.equal(1);
+    done();
+  })
+
+  describe('test for hook methods.', function(){
+    it('test1', function(){
+      beforeCount.should.equal(1);
+      beforeEachCount.should.equal(1);
+      afterEachCount.should.equal(0);
+    })
+    it('test2', function(){
+      beforeCount.should.equal(1);
+      beforeEachCount.should.equal(2);
+      afterEachCount.should.equal(1);
+    })
+    it('test3', function(){
+      beforeCount.should.equal(1);
+      beforeEachCount.should.equal(3);
+      afterEachCount.should.equal(2);
     })
   })
 })
