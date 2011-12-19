77035a704a0d20a0661c5c6d424a43aa27642ac3
ryu22e <ryu22e@gmail.com>
2011-12-19 16:27:32 +0900

    Modify type of assertion.

--- test/asynchronous-code.js
+++ test/asynchronous-code.js
@@ -1,12 +1,11 @@
-var example = require('../lib/example.js'),
-    assert = require('assert');
+var example = require('../lib/example.js');
 
 describe('example - Asynchronous code', function(){
   describe('#waitAndAdd()', function(){
     it('should return \'a + b\' when the values are \'a\' and \'b\'.', function(done){
       var ev = example.waitAndAdd(1, 2);
       ev.on('sum', function(sum) {
-        assert.equal(sum, 3);
+        sum.should.equal(3);
         done();
       });
     })
